4b73927fb1b515aebb90b55134e8e43065ce6974
mas <m@stdwtr.io>
2024-01-20 17:29:36 -0500

    Fixes

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,8 @@ import pathlib
 import nltk
 from jinja2 import Environment, FileSystemLoader
 
-try:
-    nltk.data.find('tokenizers/punkt.zip')
-except LookupError:
-    nltk.download('vader_lexicon')
-
+nltk.data.find('tokenizers/punkt.zip')
+nltk.download('vader_lexicon')
 nltk.download('averaged_perceptron_tagger')
 
 here = pathlib.Path(__file__).parent.resolve()
