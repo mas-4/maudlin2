6370627358f29b089091851ccac948795b18aadb
mas <m@stdwtr.io>
2024-02-16 17:08:14 -0500

    Headers for political wire

--- app/scrapers/politicalwire.py
+++ app/scrapers/politicalwire.py
@@ -14,6 +14,9 @@ class PoliticalWire(Scraper):
     credibility = Credibility.high
     url: str = 'https://politicalwire.com/'
     agency: str = "Political Wire"
+    headers: dict = {
+        'User-Agent': Constants.Headers.UserAgents.maudlin
+    }
 
     def setup(self, soup: Soup):
         for a in soup.find_all('a', {'href': Constants.Patterns.DATE_URL}):
