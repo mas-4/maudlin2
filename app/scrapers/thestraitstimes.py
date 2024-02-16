8fe0394a73e37e5fb2a2525df2199235ac002c09
mas <m@stdwtr.io>
2024-02-16 16:50:16 -0500

    De selenium straits times

--- app/scrapers/thestraitstimes.py
+++ app/scrapers/thestraitstimes.py
@@ -2,19 +2,22 @@ import re
 
 from bs4 import BeautifulSoup as Soup
 
-from app.constants import Bias, Credibility, Country
+from app.constants import Bias, Credibility, Country, Constants
 from app.logger import get_logger
-from app.scrapers.scraper import SeleniumScraper
+from app.scrapers.scraper import Scraper
 
 logger = get_logger(__name__)
 
 
-class TheStraitsTimes(SeleniumScraper):
+class TheStraitsTimes(Scraper):
     bias = Bias.right_center
     credibility = Credibility.mostly_factual
     url: str = 'https://www.straitstimes.com/global'
     agency: str = "The Straits Times"
     country = Country.sg
+    headers = {
+        'User-Agent': Constants.Headers.UserAgents.maudlin
+    }
 
     def setup(self, soup: Soup):
         for a in soup.find_all('a', {'class': 'stretched-link'}):
