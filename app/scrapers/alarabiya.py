e0a568465e702b242e2d72f93a1ba791c6b40794
mas <m@stdwtr.io>
2024-02-17 08:30:43 -0500

    Al Arabiya really doesn't work

--- app/scrapers/alarabiya.py
+++ app/scrapers/alarabiya.py
@@ -1,21 +1,19 @@
-import re
-
 from bs4 import BeautifulSoup as Soup
 
-from app.constants import Bias, Credibility, Country
+from app.constants import Bias, Credibility, Country, Constants
 from app.logger import get_logger
 from app.scrapers.scraper import Scraper
 
 logger = get_logger(__name__)
 
 
-class AlArabiya(Scraper):  # Disabled 403
+class AlArabiya(Scraper):
     bias = Bias.right_center
     credibility = Credibility.mixed
     url: str = 'https://english.alarabiya.net'
     agency: str = "Al Arabiya"
     country = Country.sa
-
+    headers = {'User-Agent': Constants.Headers.UserAgents.desktop_google_bot}
 
     def setup(self, soup: Soup):
         for a in soup.find_all('a', {'title': True}):
