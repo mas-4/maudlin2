360a224f51ffafaff1b21a3561bd2015ffc7634f
mas <m@stdwtr.io>
2024-02-19 12:47:23 -0500

    Add sentiment graphs

--- app/site/home.py
+++ app/site/home.py
@@ -2,16 +2,23 @@ import os
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import seaborn as sns
 
 from app import j2env
 from app.config import Config
 from app.constants import Constants
-from app.models import Session, Agency, Headline
+from app.models import Session, Agency, Headline, Article
 from app.site.common import generate_wordcloud
 from app.logger import get_logger
 from app.registry import SeleniumScrapers, TradScrapers
 
 logger = get_logger(__name__)
+
+class FileNames:
+    wordcloud = 'wordcloud.png'
+    graphs = 'graphs.png'
 
 
 class HomePage:
@@ -22,14 +29,16 @@ class HomePage:
         self.urls = {}
         self.agencies = []
         self.metrics = {}
-        self.wordcloud_filename = 'wordcloud.png'
 
     def generate(self):
         logger.info("Generating home page...")
         self.generate_home_wordcloud()
         self.generate_home_data()
+        self.render_sentiment_graphs()
         self.render_home_page()
         logger.info("...done")
+
+
 
     def render_home_page(self):
         with open(os.path.join(Config.build, 'index.html'), 'wt') as f:
@@ -38,8 +47,8 @@ class HomePage:
                 agencies=self.agencies,
                 tabledata=self.data,
                 urls=self.urls,
-                wordcloud=self.wordcloud_filename,
-                metrics=self.metrics
+                metrics=self.metrics,
+                FileNames=FileNames
             ))
 
     def generate_home_data(self):
@@ -67,12 +76,48 @@ class HomePage:
         self.metrics['mean'] = us['Sentiment'].mean()
         self.metrics['partisan_mean'] = (us['Bias'] * us['Sentiment']).mean()
 
-    def generate_home_wordcloud(self):
+    @staticmethod
+    def generate_home_wordcloud():
         with Session() as s:
             generate_wordcloud(
                 s.query(Headline).filter(
                     Headline.first_accessed > Constants.TimeConstants.midnight,
                     Headline.last_accessed > Constants.TimeConstants.five_minutes_ago
                 ).all(),
-                os.path.join(Config.build, self.wordcloud_filename)
+                str(os.path.join(Config.build, FileNames.wordcloud))
             )
+
+    def render_sentiment_graphs(self):
+        with Session() as s:
+            data = s.query(Headline.comp, Headline.last_accessed, Agency._bias)\
+                .join(Headline.article).join(Article.agency).all()
+        df = pd.DataFrame(data, columns=['Sentiment', 'Last Accessed', 'Bias'])
+        df['Last Accessed'] = pd.to_datetime(df['Last Accessed'])
+        df['PSI'] = df['Sentiment'] * df['Bias']
+        agg = df.set_index('Last Accessed').groupby(pd.Grouper(freq='D'))\
+            .agg({'Sentiment': ['mean', 'median'], 'PSI': 'mean'}).dropna().reset_index()
+        agg.columns = [' '.join(col).strip() for col in agg.columns.values]  # noqa
+        agg.rename(columns={'Last Accessed': 'Date'}, inplace=True)
+
+        fig, ax = plt.subplots(2, 2)
+        fig.set_size_inches(10, 8)
+        sns.lineplot(x='Date', y='Sentiment mean', data=agg, ax=ax[0, 0], label='Mean Sentiment')
+        sns.lineplot(x='Date', y='Sentiment median', data=agg, ax=ax[0, 1], label='Median Sentiment')
+        sns.lineplot(x='Date', y='PSI mean', data=agg, ax=ax[1, 0], label='Mean Partisan Sentiment Index')
+        sns.scatterplot(x='PSI mean', y='Sentiment mean', data=agg, ax=ax[1, 1], label='Mean Sentiment vs PSI')
+        for i in range(2):
+            for j in range(2):
+                if i == 1 and j == 1:
+                    continue
+                ax[i, j].xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))
+                ax[i, j].set_xticks(ax[i, j].get_xticks()[::2])
+                ax[i, j].set_xticklabels(ax[i, j].get_xticklabels(), rotation=45)
+        ax[1, 1].set_title('Mean Sentiment vs PSI')
+        plt.tight_layout()
+        plt.savefig(os.path.join(Config.build, FileNames.graphs))
+
+
+
+
+if __name__ == "__main__":
+    HomePage().generate()
