030ee8a34097fb602c96a36e31b2f6896b4b3131
mas <m@stdwtr.io>
2024-03-08 09:14:50 -0500

    Add selenium argument

--- main.py
+++ main.py
@@ -11,16 +11,17 @@ logger = get_logger(__name__)
 
 
 class Queue:
-    def __init__(self):
+    def __init__(self, args):
         self.threads = []
         self.seleniums = []
+        self.args = args
 
     def run(self):
         logger.info("Running threads")
         for scraper in self.threads:
             scraper.start()
 
-        if Config.run_selenium:
+        if Config.run_selenium and self.args.run_selenium:
             logger.info("Running seleniums")
             for sel in self.seleniums:
                 scraper = sel()
@@ -36,10 +37,10 @@ class Queue:
         else:
             self.threads.append(scraper())
 
-def scrape(scrapers):
+def scrape(args, scrapers):
     if not len(scrapers):
         raise ValueError("No scrapers provided")
-    queue = Queue()
+    queue = Queue(args)
     logger.info("Initializing queue")
     logger.info("Scrapers: %s", scrapers)
     for scraper in scrapers:
@@ -53,7 +54,7 @@ def main(args: argparse.Namespace):
         return
     if not args.skip_scrape:
         scrapers = [s for s in Scrapers if s.agency == args.scraper] if args.scraper else Scrapers
-        scrape(scrapers)
+        scrape(args, scrapers)
     build_site()
 
 
@@ -63,6 +64,7 @@ def get_args() -> argparse.Namespace:
     parser.add_argument('--skip-scrape', action='store_true')
     parser.add_argument('--scraper', type=str, default=None)
     parser.add_argument('--email-report', action='store_true')
+    parser.add_argument('--run-selenium', action='store_true')
     args = parser.parse_args()
     Config.dev_mode = args.dev
     return args
