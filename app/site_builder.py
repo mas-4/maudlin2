66602d435b48adc7e8960ed16a780cf0a5c6fb3d
Michael Sendker <m@standingwater.io>
2024-04-13 16:49:46 -0400

    Agencies page added back

--- app/site_builder.py
+++ app/site_builder.py
@@ -1,8 +1,7 @@
-from app.site.agencies import AgenciesPage
-from app.site.agency import generate_agency_pages
 from app.site.common import copy_assets, clear_build
 from app.site.deploy import publish_to_netlify
 from app.site.headlines import HeadlinesPage
+from app.site.agencies import AgenciesPage
 from app.site.topics import TopicsPage
 from app.utils.logger import get_logger
 
@@ -12,10 +11,10 @@ logger = get_logger(__name__)
 def build_site():
     clear_build()
     HeadlinesPage().generate()
-    # AgenciesPage().generate()
+    AgenciesPage().generate()
     # Blog().generate()
     TopicsPage().generate()
-    generate_agency_pages()
+    # generate_agency_pages()
     copy_assets()
     publish_to_netlify()
 
