a5e16860781c1c07f1da1a6f76f362a8580e30bb
mas <m@stdwtr.io>
2024-04-20 15:28:30 -0400

    Refactor headlines page

--- app/site/page_headlines.py
+++ app/site/page_headlines.py
@@ -31,37 +31,30 @@ class HeadlinesPage:
         self.dh = dh
         self.template = TemplateHandler('headlines.html', 'index.html')
         self.newsletter = TemplateHandler('newsletter.html')
+        self.context = {'title': 'Current Headlines'}
 
     def generate(self):
         logger.info("Generating headlines page...")
         df = self.filter_score_sort(self.dh.main_headline_df.copy())
-        clusters_list, df, summaries, agency_lists = self.get_summaries(df)
+        df = self.cluster_and_summarize(df)
         table_df = self.process_headlines(df)
-
-        self.newsletter.write(
-            dict(clusters=clusters_list, summaries=summaries, agency_lists=agency_lists),
-        )
-        self.template.write(
-            dict(title='Current Headlines',
-                 tabledata=table_df.values.tolist(),
-                 clusters=clusters_list,
-                 summaries=summaries,
-                 agency_lists=agency_lists),
-        )
+        self.context['tabledata'] = table_df.values.tolist()
+        self.newsletter.write(self.context)
+        self.template.write(self.context)
         logger.info("...done")
 
-    @staticmethod
-    def get_summaries(df):
+    def cluster_and_summarize(self, df):
         n_samples_per_cluster = 5
         df['processed'] = df['title'].apply(lambda x: prepare(x, pipeline))
-        cosine_sim = prepare_cosine(df['processed'])
-        clusters = form_clusters(cosine_sim, min_samples=n_samples_per_cluster, threshold=0.5)
-        df = label_clusters(df, clusters)
-        # group each cluster, concatenate the processed text, and summarize with nlp
-        summaries = {}
-        for key, group in df.groupby('cluster'):
-            text = ' '.join(group['title'])[:1024]
-            summaries[key] = summarizer(text, min_length=10, max_length=50, do_sample=True)[0]['summary_text']
+        df = label_clusters(
+            df,
+            form_clusters(
+                prepare_cosine(df['processed']),
+                min_samples=n_samples_per_cluster,
+                threshold=0.5
+            )
+        )
+        self.summarize(df)
         df['text_length'] = df['processed'].str.len()
         df.sort_values(by='text_length', ascending=False, inplace=True)
         df.drop_duplicates(subset=['cluster', 'agency'], keep='first', inplace=True)
@@ -76,7 +69,17 @@ class HeadlinesPage:
             for a in sorted(cluster['data'], key=lambda x: x['agency']):
                 hrefs.append(f'<a href="{a["url"]}">{a["agency"]}</a>')
             agency_lists[cluster['cluster']] = ', '.join(hrefs)
-        return clusters_list, df, summaries, agency_lists
+        self.context['clusters'] = clusters_list
+        self.context['agency_lists'] = agency_lists
+        return df
+
+    def summarize(self, df):
+        # group each cluster, concatenate the processed text, and summarize with nlp
+        summaries = {}
+        for key, group in df.groupby('cluster'):
+            text = ' '.join(group['title'])[:1024]
+            summaries[key] = summarizer(text, min_length=10, max_length=50, do_sample=True)[0]['summary_text']
+        self.context['summaries'] = summaries
 
     @staticmethod
     def process_headlines(df):
@@ -88,13 +91,13 @@ class HeadlinesPage:
         df['title'] = df.apply(format_title, axis=1)
 
         def format_topic(x):
+            # Truncate headline_df['title'] to 255 characters and append a ... if it is longer
             if not x.topic:
                 return ''
             topic_file = x.topic.replace(' ', '_') + '.html'
             return f'<a href="{topic_file}.html">{x.topic}</a>'
 
         df['topic'] = df.apply(format_topic, axis=1)
-        # Truncate headline_df['title'] to 255 characters and append a ... if it is longer
         df = df[
             ['title', 'first_accessed', 'last_accessed', 'score', 'topic', 'vader_compound', 'afinn']
         ]
