0ad5a252c6ad82bc03085be817eec5b75ae4cb3c
mas <m@stdwtr.io>
2024-04-20 11:40:48 -0400

    Add argument to setup db

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,8 +4,11 @@ from app.utils.config import Config
 from app.utils.constants import Constants
 
 
+def pytest_addoption(parser):
+    parser.addoption('--setup-db', action='store_true', help='Move tests/test.db to data/data.db')
+
+
 def pytest_sessionstart(session):
-    # move tests/test.db to data/data.db
-    Config.db = os.path.join(Config.data, 'data.db')
+    if session.config.getoption('--setup-db'):
+        os.rename(os.path.join(Constants.Paths.ROOT, 'tests', 'test.db'), Config.db_file_path)
     Config.debug = True
-    os.rename(os.path.join(Constants.Paths.ROOT, 'tests', 'test.db'), Config.db)
